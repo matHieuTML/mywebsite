ca50d5a0dcbdda5366e3ee98a019976bd2ec56f3
Mathieu <gaucher.mathieu@icloud.com>
2025-04-12 17:31:40 +0200

    add: Matomo script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, Montserrat } from 'next/font/google'
+import Script from 'next/script';
 import "./globals.css";
 import { defaultMetadata } from './config/metadata';
 import Header from './components/Header'
@@ -27,6 +28,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" className={`${spaceGrotesk.variable} ${montserrat.variable}`}>
+      <head>
+        <Script strategy="afterInteractive" id="matomo-analytics">{`
+          var _paq = window._paq = window._paq || [];
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="https://mathieugaucher.matomo.cloud/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src='https://cdn.matomo.cloud/mathieugaucher.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        `}</Script>
+      </head>
       <body>
         <div className="layout-container">
           <Header />
